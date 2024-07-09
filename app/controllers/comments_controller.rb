4a4df6244d931cbcd7af35cac5b160f118263808
dylansarikas <dylan.sarikas@gmail.com>
2024-07-09 14:44:20 -0500

    ajaxifies comment edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,9 @@ class CommentsController < ApplicationController
   def edit
     respond_to do |format|
       format.html
+      format.js do
+        render template: "comments/edit"
+      end
     end
   end
 
